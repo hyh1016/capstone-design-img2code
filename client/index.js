67bf797494a8cc85ea4e2f0aac0e6c8d17a93172
YIHYUN HA <hihyun16@gmail.com>
2022-09-18 15:54:25 +0900

    Add event format

---
+++ client/index.js
@@ -0,0 +1,21 @@
+const init = () => {
+    const button = document.getElementById('upload-btn');
+    button.addEventListener('click', uploadButtonEvent);
+}
+
+// TODO
+const uploadButtonEvent = () => {
+    // Image 입력을 받아서
+
+
+    // pix2code API 서버에 요청해서 html 획득
+
+
+    // 받아온 html을 File로 생성 (File명 == Image명)
+
+    // 다운로드 이벤트
+
+
+}
+
+init();
