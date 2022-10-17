9667d6153e44deab61bbf580c8f735b059183c8d
sidereumare <48210134+sidereumare@users.noreply.github.com>
2022-10-17 20:43:01 +0900

    평균 file, tag accuracy 계산함수 완성

--- Analysis.py
+++ Analysis.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # dataPath = "pix2code/datasets/pix2code_datasets/web/all_data"
-dataPath = "dataGenerator/data/dsl/"
+# dataPath = "dataGenerator/data/dsl/"
 
 def getCenterPosition(driver, element):
     location = element.location
@@ -24,7 +24,7 @@ def getCenterPosition(driver, element):
 def gerElementSize(driver, element):
     size = element.size
     return size['width']*size['height']
-def plotTagData(path):
+def plotTagData(dataPath):
     dataset = Dataset()
     dataset.load(dataPath, generate_binary_sequences=True)
     dic = dict()
@@ -72,7 +72,7 @@ def plotCenterPos(path):
 
     return centerPosData
 
-def plotTagSize(path):
+def plotTagSize(dataPath):
     driver = webdriver.Chrome()
     htmls = [f for f in os.listdir(dataPath) if f.endswith('.html')]
     # 태그별 크기 정보 수집
@@ -118,7 +118,7 @@ def plotTagSize(path):
     return sizeData
 
 
-def colorTagPlot(path):
+def colorTagPlot(dataPath):
     colors = {'rgba(51, 51, 51, 1)':'black', 'rgba(59, 130, 246, 1)':'blue', 'rgba(245, 245, 245, 1)':'white', 'rgba(249, 115, 22, 1)':'orange', 'rgba(239, 68, 68, 1)':'red', 'rgba(34, 197, 94, 1)':'green'}
     driver = webdriver.Chrome()
     htmls = [f for f in os.listdir(dataPath) if f.endswith('.html')]
@@ -154,38 +154,101 @@ def colorTagPlot(path):
 
     return colorData
 
-# 문자열 유사도 계산
-from difflib import SequenceMatcher
-
-# 파일 유사도 계산
-def fileSimilarity(path1, path2):
-    with open(path1, 'r') as f:
+def getTokenizedData(path):
+    with open(path, 'r') as f:
         a = f.readlines()
-    with open(path2, 'r') as f:
-        b = f.readlines()
-
     a_token_sequence = []
     for line in a:
         line = line.replace(",", " ,").replace("\n", " \n")
         tokens = line.split(" ")
         for token in tokens:
             a_token_sequence.append(token)
-    
-    b_token_sequence = []
-    for line in b:
-        line = line.replace(",", " ,").replace("\n", " \n")
-        tokens = line.split(" ")
-        for token in tokens:
-            b_token_sequence.append(token)
+    return a_token_sequence
 
+# 문자열 유사도 계산
+from difflib import SequenceMatcher
+
+# 파일 유사도 계산
+def fileSimilarity(path1, path2):
+    a = getTokenizedData(path1)
+    b = getTokenizedData(path2)
     # 배열 유사 리턴
     return SequenceMatcher(None, a, b).ratio()
+
+
+# get lcs using backtracking
+# return correct array
+def getLCS(a, b):
+    # 2차원 배열 생성
+    dp = [[0 for _ in range(len(b)+1)] for _ in range(len(a)+1)]
+    # dp[i][j] : a[:i]와 b[:j]의 lcs 길이
+    for i in range(1, len(a)+1):
+        for j in range(1, len(b)+1):
+            if a[i-1]==b[j-1]:
+                dp[i][j] = dp[i-1][j-1]+1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    # 해당 문자가 lcs에 포함되는지 확인
+    # backtracking
+    i, j = len(a), len(b)
+    isInLcs = [False for _ in range(len(a))]
+    while i>0 and j>0:
+        if a[i-1]==b[j-1]:
+            isInLcs[i-1] = True
+            i -= 1
+            j -= 1
+        else:
+            if dp[i-1][j]>dp[i][j-1]:
+                i -= 1
+            else:
+                j -= 1
+    return isInLcs
+
+
+# 태그별 정답률
+def getAccuracyPerTag(path1, path2):
+    a_token_sequence = getTokenizedData(path1)
+    b_token_sequence = getTokenizedData(path2)
+    # lcs
+    lcs = getLCS(a_token_sequence, b_token_sequence)
+    # 태그별 정답률
+    tagAccuracy = dict()
+    for i, token in enumerate(a_token_sequence):
+        if token not in tagAccuracy:
+            # [정답 개수, 전체 개수]
+            tagAccuracy[token] = [0, 0]
+        if lcs[i]:
+            tagAccuracy[token][0] += 1
+        tagAccuracy[token][1] += 1
+    return tagAccuracy
+
 
 # 폴더 한번에 유사도 계산
 def batchFileSimilarity(path):
     guis = [f for f in os.listdir(path+'dsl/') if f.endswith('.gui')]
+
+    fileAccuracy = []
+    tagAccuracy = dict()
     for i, name in enumerate(guis):
+        # 파일별 유사도 계산
         path1 = os.path.join(path+'dsl/', name)
         path2 = os.path.join(path+'predicted/', name)
-        print(path1, path2, fileSimilarity(path1, path2))
+        fileAccuracy.append(fileSimilarity(path1, path2))
+
+        # 태그별 정답률
+        tagAccuracyPerFile = getAccuracyPerTag(path1, path2)
+        for tag in tagAccuracyPerFile:
+            if tag not in tagAccuracy:
+                tagAccuracy[tag] = [0, 0]
+            tagAccuracy[tag][0] += tagAccuracyPerFile[tag][0]
+            tagAccuracy[tag][1] += tagAccuracyPerFile[tag][1]
+    return fileAccuracy, tagAccuracy
+
+
+
+if __name__ == '__main__':
+    fileAccuracy, tagAccuracy = batchFileSimilarity('dataGenerator/data_/')
+    print('평균 file accuracy :', sum(fileAccuracy)/len(fileAccuracy))
+    plt.bar(tagAccuracy.keys(), [tagAccuracy[tag][0]/tagAccuracy[tag][1] for tag in tagAccuracy])
+    plt.show()
 
