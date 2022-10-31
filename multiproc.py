55b054f291e53aec8fc7747a6454ee2026a59fa4
sidereumare <48210134+sidereumare@users.noreply.github.com>
2022-10-31 16:00:36 +0900

    multiproc 파일 명 한곳에서 입력하게 수정

--- multiproc.py
+++ multiproc.py
@@ -7,7 +7,7 @@ from pix2code.model.classes.model.pix2code_v1 import *
 from pix2code.model.classes.model.pix2code_v1_Bi_GRU import *
 from pix2code.model.classes.model.pix2code_v2_GRU_dense import *
 
-PROCESS_COUNT = 32
+PROCESS_COUNT = 64
 
 def predict_greedy_multi(input_img, inQueue:Queue, outQueue:Queue, id, voc:Vocabulary, input_shape, output_size, context_length, require_sparse_label=True, sequence_length=150, verbose=False):
     current_context = [voc.vocabulary[PLACEHOLDER]] * (context_length - 1)
@@ -26,12 +26,8 @@ def predict_greedy_multi(input_img, inQueue:Queue, outQueue:Queue, id, voc:Vocab
         ##################
         inQueue.put((input_img, np.array([current_context]), id))
         probas = outQueue.get()
-        # while True:
-        #     if not outQueue.empty():
-        #         probas = outQueue.get()
-        #         break
         ##################
-
+        # 원본 코드
         # probas = model.predict(input_img, np.array([current_context]))
         prediction = np.argmax(probas)
         out_probas.append(probas)
@@ -85,18 +81,15 @@ def predictDsl(input_path, output_path, trained_weights_path, inQueue:Queue, out
         result = result.replace('<START>', '').replace('<END>', '').replace('{', ' {').replace(',', ', ')
         with open(f'{output_path}{file_name}.gui', 'w') as f:
             f.write(result)
-        # print('Predicted DSL for {}.'.format(input_file))
-        # print(result)
-        # print('-----------------------')
 
 
-def worker(trained_weights_path:str, inQueue:mp.Queue, outQueue:mp.Queue):
+def worker(trained_weights_path:str, weights_name:str, inQueue:mp.Queue, outQueue:mp.Queue):
     meta_dataset = np.load("{}/meta_dataset.npy".format(trained_weights_path), allow_pickle=True)
     input_shape = meta_dataset[0]
     output_size = meta_dataset[1]
 
     model = pix2code_v2_GRU_dense(input_shape, output_size, trained_weights_path)
-    model.load('pix2code_v2_GRU_dense')
+    model.load(weights_name)
     
     input_imgs = np.empty((0, 256, 256, 3))
     current_contexts = np.empty((0, 48, 23))
@@ -127,7 +120,7 @@ def worker(trained_weights_path:str, inQueue:mp.Queue, outQueue:mp.Queue):
             ids = []
             cnt = 0
 
-def main_process(inQueue:mp.Queue, outQueue:list, trained_weights_path:str, data_path:str):
+def main_process(inQueue:mp.Queue, outQueue:list, trained_weights_path:str, weights_name:str, data_path:str):
     meta_dataset = np.load("{}/meta_dataset.npy".format(trained_weights_path), allow_pickle=True)
     input_shape = meta_dataset[0]
     output_size = meta_dataset[1]
@@ -136,15 +129,13 @@ def main_process(inQueue:mp.Queue, outQueue:list, trained_weights_path:str, data
 
     procs = []
     for i in range(PROCESS_COUNT):
-        # predictDsl(input_path, output_path, file_lists[i], inQueue, outQueue[i], input_shape, output_size, i)
-        # print(input_path, output_path, inQueue, outQueue[i], input_shape, output_size, i)
         p = Process(target=predictDsl, args=(input_path, output_path, trained_weights_path, inQueue, outQueue[i], input_shape, output_size, i, ))
         p.start()
         procs.append(p)
     
     import time
     time.sleep(1)
-    workerProc = Process(target=worker, args=(trained_weights_path, inQueue, outQueue))
+    workerProc = Process(target=worker, args=(trained_weights_path, weights_name, inQueue, outQueue))
     workerProc.start()
 
     for p in procs:
@@ -153,8 +144,9 @@ def main_process(inQueue:mp.Queue, outQueue:list, trained_weights_path:str, data
     workerProc.join()
 
 if __name__ == '__main__':
-    trained_weights_path='pix2code/bin/tag_extension/v2_GRU_dense'
-    data_path = 'dataGenerator/data/7'
+    trained_weights_path='pix2code/bin/tag_extension/8_GRU_dense_15000data'
+    data_path = 'dataGenerator/data/8'
+    weights_name = 'pix2code_v2_GRU_dense'
     inQueue = mp.Queue()
     outQueue = [mp.Queue() for _ in range(PROCESS_COUNT)]
-    main_process(inQueue, outQueue, trained_weights_path, data_path)
+    main_process(inQueue, outQueue, trained_weights_path, weights_name, data_path)
